3b03ac63b8849a8b63c0a27b58e40352e8cee277
Rajeev <scraptorajeev@gmail.com>
2022-01-27 23:37:26 +0530

    Form for conversion

--- src/components/snet-conversion-input/index.jsx
+++ src/components/snet-conversion-input/index.jsx
@@ -1,15 +1,16 @@
 import { Stack, Typography } from '@mui/material';
 import WalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import propTypes from 'prop-types';
 import BlockchainDropdown from './BlockchainDropdown';
 import InputWithAssetDropdown from './InputWithAssetDropdown';
 import { styles } from './styles';
 
-const SnetConversionOptions = () => {
+const SnetConversionOptions = ({ direction }) => {
   return (
     <>
       <Stack spacing={1} direction="row" alignItems="center" marginBottom={2} justifyContent="space-between">
         <Stack spacing={1} direction="row" alignItems="center">
-          <Typography variant="body2">FROM</Typography>
+          <Typography variant="body2">{direction}</Typography>
           <BlockchainDropdown />
         </Stack>
         <Stack spacing={1} direction="row" alignItems="center">
@@ -20,6 +21,10 @@ const SnetConversionOptions = () => {
       <InputWithAssetDropdown />
     </>
   );
+};
+
+SnetConversionOptions.propTypes = {
+  direction: propTypes.string.isRequired
 };
 
 export default SnetConversionOptions;
--- src/pages/Converter/ConverterForm.jsx
+++ src/pages/Converter/ConverterForm.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+import Fab from '@mui/material/Fab';
+import SwapIcon from '@mui/icons-material/SwapVert';
+import Stack from '@mui/material/Stack';
 import SnetConversionOptions from '../../components/snet-conversion-input';
 import SnetPaper from '../../components/snet-paper';
+import SnetButton from '../../components/snet-button';
 
 const ConverterForm = () => {
+  const [isConversionDisabled] = useState(true);
+
+  const onClickConvert = () => {
+    // TODO: Implement conversion logic
+  };
+
   return (
     <SnetPaper>
-      <SnetConversionOptions />
+      <SnetConversionOptions direction="FROM" />
+      <Stack direction="row" alignItems="center" justifyContent="center" padding={4}>
+        <Fab aria-label="swap-icon" color="primary">
+          <SwapIcon />
+        </Fab>
+      </Stack>
+      <SnetConversionOptions direction="TO" />
+      <Stack direction="row" alignItems="center" justifyContent="center" padding={4}>
+        <SnetButton disabled={isConversionDisabled} name="Convert" onClick={onClickConvert} />
+      </Stack>
     </SnetPaper>
   );
 };
