aa0fffb57c6b293002d4516a48c6d333d1cf918c
Rajeev <scraptorajeev@gmail.com>
2022-02-10 09:03:33 +0530

    Web3 integration with web3Modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import { Web3ReactProvider } from '@web3-react/core';
-import { getLibrary } from './components/snet-wallet-connector/wallet-connectors';
 import Router from './router';
 
-const App = () => (
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <Router />
-  </Web3ReactProvider>
-);
+const App = () => <Router />;
 
 export default App;
--- src/components/snet-wallet-connector/style.js
+++
@@ -1,6 +0,0 @@
-const style = {
-  divider: { marginBottom: 4, marginTop: 4 },
-  walletConnectButton: { width: '100%', display: 'inline-block', minWidth: 320 }
-};
-
-export default style;
