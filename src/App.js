ca0ac7c8ba5f4f18a36e51c5060ba2305f670f4f
Rajeev <scraptorajeev@gmail.com>
2022-03-11 14:47:46 +0530

    Google Analytics Integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Router from './router';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => <Router />;
 
