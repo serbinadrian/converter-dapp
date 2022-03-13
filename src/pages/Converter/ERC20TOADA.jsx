a36642018adb20c62983bf9d96fcfb850cb72a8f
Rajeev <scraptorajeev@gmail.com>
2022-03-13 13:08:28 +0530

    Display popup after conversion initialization

--- src/pages/Converter/ERC20TOADA.jsx
+++ src/pages/Converter/ERC20TOADA.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { Stack } from '@mui/material';
-import { toUpper, isEmpty } from 'lodash';
+import { toUpper, isEmpty, isNil } from 'lodash';
 import SnetPaper from '../../components/snet-paper';
 import { useConverterHook } from './hooks/ConverterHook';
 import ConversionFormLoader from './ConversionFormLoader';
@@ -11,6 +11,7 @@ import { useERC20TokenHook } from './hooks/ERC20TokenHook';
 import { availableBlockchains } from '../../utils/ConverterConstants';
 import SnetAlert from '../../components/snet-alert';
 import SnetLoader from '../../components/snet-loader';
+import SnetConversionStatus from '../../components/snet-conversion-status';
 
 const ERC20TOADA = () => {
   const { blockchains, wallet } = useSelector((state) => state);
@@ -35,7 +36,8 @@ const ERC20TOADA = () => {
     updateWalletBalance,
     walletBalance
   } = useConverterHook();
-  const { fetchWalletBalance, getAllowanceInfo, conversionEnabled, authorizationRequired, approveSpendLimit, loader, burnERC20Tokens } = useERC20TokenHook();
+  const { fetchWalletBalance, getAllowanceInfo, conversionEnabled, authorizationRequired, approveSpendLimit, loader, burnERC20Tokens, txnInfo } =
+    useERC20TokenHook();
   const { toAddress } = wallet;
 
   const getBalanceFromWallet = async () => {
@@ -65,9 +67,9 @@ const ERC20TOADA = () => {
 
   const onClickConvert = async () => {
     try {
-      const txnLink = await burnERC20Tokens(fromTokenPair.id, fromAndToTokenValues.fromValue, toAddress);
+      await burnERC20Tokens(fromTokenPair.id, fromAndToTokenValues.fromValue, toAddress);
     } catch (error) {
-      console.log('onClickAuhorize', error);
+      console.log('onClickConvert', error);
     }
   };
 
@@ -80,40 +82,50 @@ const ERC20TOADA = () => {
   }
 
   return (
-    <SnetPaper>
-      <SnetLoader dialogBody={loader.message} onDialogClose={() => {}} isDialogOpen={loader.isLoading} dialogTitle={loader.title} />
-      <TokenPairs
-        fromBlockchains={fromBlockchains}
-        fromSelectedBlockchain={fromSelectedBlockchain}
-        toBlockchains={toBlockchains}
-        toSelectedBlockchain={toSelectedBlockchain}
-        handleFromBlockchainSelection={handleFromBlockchainSelection}
-        handleToBlockchainSelection={handleToBlockchainSelection}
-        onSwapBlockchain={swapBlockchains}
-        fromTokenPair={fromTokenPair}
-        toTokenPair={toTokenPair}
-        onSelectingFromToken={onSelectingFromToken}
-        onSelectingToToken={onSelectingToToken}
-        onUseFullamount={onUseFullamount}
-        fromInputChange={handleFromInputChange}
-        toInputChange={handleToInputChange}
-        fromInputValue={fromAndToTokenValues.fromValue}
-        toInputValue={fromAndToTokenValues.toValue}
-        conversionFee={conversionCharge.amount}
-        conversionSymbol={conversionCharge.symbol}
-        walletBalance={walletBalance.balance}
-        walletTokenSymbol={walletBalance.symbol}
+    <>
+      <SnetConversionStatus
+        isDialogOpen={!isNil(txnInfo.txnLink)}
+        title="Conversion Status"
+        amount={txnInfo.txnAmount}
+        tokenName={txnInfo.tokenSymbol}
+        link={txnInfo.txnLink ?? ''}
+        onDialogClose={() => {}}
       />
-      {error.error ? (
-        <Stack marginTop={4}>
-          <SnetAlert error={error.message} />
+      <SnetPaper>
+        <SnetLoader dialogBody={loader.message} onDialogClose={() => {}} isDialogOpen={loader.isLoading} dialogTitle={loader.title} />
+        <TokenPairs
+          fromBlockchains={fromBlockchains}
+          fromSelectedBlockchain={fromSelectedBlockchain}
+          toBlockchains={toBlockchains}
+          toSelectedBlockchain={toSelectedBlockchain}
+          handleFromBlockchainSelection={handleFromBlockchainSelection}
+          handleToBlockchainSelection={handleToBlockchainSelection}
+          onSwapBlockchain={swapBlockchains}
+          fromTokenPair={fromTokenPair}
+          toTokenPair={toTokenPair}
+          onSelectingFromToken={onSelectingFromToken}
+          onSelectingToToken={onSelectingToToken}
+          onUseFullamount={onUseFullamount}
+          fromInputChange={handleFromInputChange}
+          toInputChange={handleToInputChange}
+          fromInputValue={fromAndToTokenValues.fromValue}
+          toInputValue={fromAndToTokenValues.toValue}
+          conversionFee={conversionCharge.amount}
+          conversionSymbol={conversionCharge.symbol}
+          walletBalance={walletBalance.balance}
+          walletTokenSymbol={walletBalance.symbol}
+        />
+        {error.error ? (
+          <Stack marginTop={4}>
+            <SnetAlert error={error.message} />
+          </Stack>
+        ) : null}
+        <Stack direction="row" alignItems="center" spacing={2} justifyContent="center" padding={4}>
+          <SnetButton disabled={!conversionEnabled} name="Convert" onClick={onClickConvert} />
+          <SnetButton disabled={!authorizationRequired} name="Authorize" onClick={onClickAuhorize} />
         </Stack>
-      ) : null}
-      <Stack direction="row" alignItems="center" spacing={2} justifyContent="center" padding={4}>
-        <SnetButton disabled={!conversionEnabled} name="Convert" onClick={onClickConvert} />
-        <SnetButton disabled={!authorizationRequired} name="Authorize" onClick={onClickAuhorize} />
-      </Stack>
-    </SnetPaper>
+      </SnetPaper>
+    </>
   );
 };
 
