ae43a895d853f6658ec87e96cdb2b9bbb0b42de0
dasari-ananya <ananya.sgrdasari@gmail.com>
2022-04-19 18:14:31 +0530

    home page info box show only when both the wallets are connected

--- src/pages/Converter/PendingTxnAlert.jsx
+++ src/pages/Converter/PendingTxnAlert.jsx
@@ -1,18 +1,21 @@
-import { Grid, Link } from '@mui/material';
+import { Link } from '@mui/material';
+import { isEmpty, size } from 'lodash';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import SnetAlert from '../../components/snet-alert';
 import { useWalletHook } from '../../components/snet-wallet-connector/walletHook';
 import { getPendingConversionCount } from '../../utils/HttpRequests';
-import styles from './styles';
 
 const PendingTxnAlert = () => {
   const [isPendingTxns, setPendingTxns] = useState(false);
-  const { getWalletAddress, address } = useWalletHook();
+  const { getWalletAddress } = useWalletHook();
+  const state = useSelector((state) => state);
+  const { wallets } = state.wallet;
 
   const fetchPendingTransactionCounts = async () => {
     try {
       const wallet = await getWalletAddress();
-      if (wallet) {
+      if (!isEmpty(wallets)) {
         const { each } = await getPendingConversionCount(wallet);
         const pendingTxnCount = (each.PROCESSING ?? 0) + (each.CLAIM_INITIATED ?? 0) + (each.USER_INITIATED ?? 0) + (each.WAITING_FOR_CLAIM ?? 0);
         setPendingTxns(pendingTxnCount > 0);
@@ -23,15 +26,12 @@ const PendingTxnAlert = () => {
   };
 
   useEffect(() => {
-    fetchPendingTransactionCounts();
-  }, [address]);
+    if (size(wallets) > 1) {
+      fetchPendingTransactionCounts();
+    }
+  }, [wallets]);
 
-  const link = (
-    <Link underline="none" href="/transactions">
-      Transactions
-    </Link>
-  );
-  return isPendingTxns ? (
+  return isPendingTxns && size(wallets) > 1 ? (
     <SnetAlert
       error={
         <p>
