c59d80afad6009cab2e692e473e643f6cd361d99
dasari-ananya <ananya.sgrdasari@gmail.com>
2022-05-12 20:09:09 +0530

    waiting icon on stepper added

--- src/components/snet-ada-eth-conversion-form/SnetAdaEthSteps.jsx
+++ src/components/snet-ada-eth-conversion-form/SnetAdaEthSteps.jsx
@@ -2,8 +2,8 @@ import { useSelector } from 'react-redux';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-// import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
-// import { styled } from '@mui/material/styles';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
+import { styled } from '@mui/material/styles';
 import propTypes from 'prop-types';
 import { conversionStatuses } from '../../utils/ConverterConstants';
 import { useStyles } from './styles';
@@ -12,35 +12,43 @@ const SnetAdaEthSteps = ({ steps, activeStep }) => {
   const classes = useStyles();
   const { conversion } = useSelector((state) => state.tokenPairs.conversionOfAdaToEth);
 
-  const isStepFailed = () => {
-    return conversion.status === conversionStatuses.EXPIRED;
+  const isStepFailed = (step) => {
+    if (conversion.status === conversionStatuses.EXPIRED) {
+      return step === 1;
+    }
+    return null;
   };
 
-  // const ColorlibStepIconRoot = styled('div')(() => ({
-  //   backgroundColor: '#F18D5A',
-  //   color: '#fff',
-  //   width: 28,
-  //   height: 28,
-  //   display: 'flex',
-  //   borderRadius: '50%',
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   '& svg': { fontSize: 18 }
-  // }));
+  const ColorlibStepIconRoot = styled('div')(() => ({
+    backgroundColor: '#F18D5A',
+    color: '#fff',
+    width: 28,
+    height: 28,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& svg': { fontSize: '18px !important' }
+  }));
 
-  // const waitingIcon = () => {
-  //   return (
-  //     <ColorlibStepIconRoot>
-  //       <HourglassEmptyIcon />
-  //     </ColorlibStepIconRoot>
-  //   );
-  // };
-
-  // <StepLabel StepIconComponent={waitingIcon}>{label}</StepLabel>
+  const waitingIcon = () => {
+    return (
+      <ColorlibStepIconRoot>
+        <HourglassEmptyIcon />
+      </ColorlibStepIconRoot>
+    );
+  };
 
   return (
     <Stepper activeStep={activeStep} alternativeLabel className={classes.stepper}>
-      {steps.map(({ step, label }) => {
+      {steps.map(({ step, label, progress }) => {
+        if (progress === 'PROCESSING') {
+          return (
+            <Step key={step}>
+              <StepLabel StepIconComponent={waitingIcon}>{label}</StepLabel>
+            </Step>
+          );
+        }
         const labelProps = {};
         if (isStepFailed(step)) {
           labelProps.error = true;
