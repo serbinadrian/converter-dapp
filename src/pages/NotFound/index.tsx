16e125cab7e6293b4a42d947821ce0fc233c6414
Andrew Serbin <serbinadrian@outlook.com>
2023-09-06 10:35:03 +0300

    Updated 'construction' and 'not found' pages

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,18 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const NotFound = (): React.ReactElement => {
     return (
         <React.Fragment>
-            <h1>NotFound</h1>
+            <Box className="not-found">
+                <img
+                    src={
+                        require("../../assets/images/background/not_found.svg")
+                            .default
+                    }
+                    alt="Not Found"
+                />
+            </Box>
         </React.Fragment>
     );
 };
